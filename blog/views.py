f10307de618b27af688e20da20125a30ffde8bb2
Khoirulamirin Nawee <6710515013@cn103>
2025-09-14 03:18:21 +0700

    add structure

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        "posts" : posts  
+    }
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
